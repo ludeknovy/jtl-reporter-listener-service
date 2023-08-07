b785104537ff0502c2e107b2a60a658247906f83
Ludek Novy <13610612+ludeknovy@users.noreply.github.com>
2023-08-07 22:40:20 +0200

    log samples v3 (#37)
    Co-authored-by: Luděk Nový <git@github.com:ludeknovy/jtl-reporter-listener-service.git>

--- src/jsonSchema/saveDataBodySchema.ts
+++ src/jsonSchema/saveDataBodySchema.ts
@@ -125,3 +125,70 @@ export const saveDataBodySchemav2 = {
     },
     required: ["itemId", "samples"],
 }
+
+export const saveDataBodySchemav3 = {
+    title: "Performance measurement",
+    type: "object",
+    properties: {
+        itemId: {
+            type: "string",
+        },
+        samples: {
+            type: "array",
+            maxItems: 500,
+            items: {
+                type: "object",
+                required: ["timeStamp", "elapsed", "label", "responseCode",
+                    "responseMessage", "success", "bytes", "allThreads", "latency", "connect"],
+                properties: {
+                    timestamp: {
+                        type: "number",
+                    },
+                    elapsed: {
+                        type: "number",
+                    },
+                    label: {
+                        type: "string",
+                    },
+                    responseCode: {
+                        type: "string",
+                    },
+                    responseMessage: {
+                        type: "string",
+                    },
+                    threadName: {
+                        type: "string",
+                    },
+                    success: {
+                        type: "boolean",
+                    },
+                    bytes: {
+                        type: "number",
+                    },
+                    sentBytes: {
+                        type: "number",
+                    },
+                    grpThreads: {
+                        type: "number",
+                    },
+                    allThreads: {
+                        type: "number",
+                    },
+                    latency: {
+                        type: "number",
+                    },
+                    hostname: {
+                        type: "string",
+                    },
+                    connect: {
+                        type: "number",
+                    },
+                    failureMessage: {
+                        type: "string",
+                    },
+                },
+            },
+        },
+    },
+    required: ["itemId", "samples"],
+}
